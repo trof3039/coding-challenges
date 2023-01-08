d2add4e95cce1d55f95f67442c262fe50c74c401
Anton Trofimenko <anton.t@amfg.ai>
2023-01-08 17:05:15 +0200

    solution was shorted

--- leetcode/medium/gas-station.js
+++ leetcode/medium/gas-station.js
@@ -16,21 +16,16 @@ const gas1 = [2,3,4]
 const cost1 = [3,4,3]
 
 const canCompleteCircuit = (gas, cost) => {
-    let start = 0
-    let end = 0
-    let g = gas[start]
-
+    let start = 0, end = 0,  g = gas[start]
     for (let i = 0; i < gas.length; i++) {
         if (g - cost[end] < 0) {
             start = start === 0 ? gas.length - 1 : start - 1
             g += gas[start] - cost[start]
-        } else {
-            g += gas[end + 1] - cost[end]
-            end++
-        }
+        } else g += gas[end + 1] - cost[end], end++
     }
     return g - gas[start] < 0 ? -1 : start
 };
+
 
 console.log(canCompleteCircuit(gas, cost))
 console.log(canCompleteCircuit(gas1, cost1))
