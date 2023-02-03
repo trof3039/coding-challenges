ca6a7bc241588e3c9b1bdfcee63774d0e2b3df16
anton <anton.t@amfg.ai>
2023-02-03 16:57:45 +0200

    added a problem with a solution

---
+++ leetcode/medium/zigzag-conversion.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/zigzag-conversion/
+
+
+// good sulution, idk why it's not in top 90%
+// Runtime 100 ms. Beats 56.87%.
+// Memory 48.1 MB. Beats 50.60%.
+const convert = (s, numRows) => {
+    if (numRows === 1) return s
+    const rows = Array.from({length: numRows}, () => [])
+    let moveDown = true
+    let row = 0
+    for (let i = 0; i < s.length; i++) {
+        rows[row].push(s[i])
+        if (row === 0) row = 1, moveDown = true
+        else if (row === rows.length - 1) row = rows.length - 2, moveDown = false
+        else if (moveDown) row++
+        else row--
+    }
+    return rows.flat().join('')
+};
+
+// const s = "PAYPALISHIRING", numRows = 3, output = 'PAHNAPLSIIGYIR'
+const s = "PAYPALISHIRING", numRows = 4, output = 'PINALSIGYAHRPI'
+
+console.log(convert(s, numRows) === output)
