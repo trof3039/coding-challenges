b7dad571237aaf954ed8b48f5d709a791c21cd5d
Anton <trof3039@gmail.com>
2023-02-04 16:02:47 +0200

    refactoring

--- leetcode/medium/permutation-in-string.js
+++ leetcode/medium/permutation-in-string.js
@@ -2,15 +2,15 @@
 
 // not bad solution
 // Runtime 128 ms. Beats 53.17%.
-// Memory 48.8 MB. Beats 28.77%.
+// Memory 48.8 MB. Beats 32.38%.
 const checkInclusion = (s1, s2) => {
     const letters = s1.split('').reduce((acc, l) => (acc[l] = (acc[l] || 0) + 1, acc), {})
     for (let i = 0; i < s2.length; i++) {
         const letterToInclude = s2[i]
         const letterToExclude = s2[i - s1.length]
-        
+
         if (letterToExclude && letterToInclude === letterToExclude) continue
-        
+
         const toIncludeCount = letters[letterToInclude]
 
         if (toIncludeCount === 1) delete letters[letterToInclude]
@@ -18,14 +18,14 @@ const checkInclusion = (s1, s2) => {
 
         if (i >= s1.length) {
             const toExcludeCount = letters[letterToExclude]
-            
+
             if (toExcludeCount === -1) delete letters[letterToExclude]
             else letters[letterToExclude] = (toExcludeCount || 0) + 1
         }
 
-        if (i >= s1.length - 1 && Object.keys(letters).length === 0) return true 
+        if (i >= s1.length - 1 && Object.keys(letters).length === 0) return true
     }
-    return false    
+    return false
 }
 
 const s1 = "aba", s2 = "eidbaooo"
